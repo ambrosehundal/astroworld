5d3a9a0a7279030bc955ea8731ca064e611e921f
Ambrose Hundal <amshunda@ucsc.edu>
2021-02-03 09:56:29 -0800

    states added for photo and dateinput

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Mars from './components/Mars'
+import DateInput from './components/DateInput'
+import Photo from './components/Photo'
 
 function App() {
+  
+  changeDate = e =>{
+    e.preventDefault();
+    
+  }
+  
+  state = {
+    date: "",
+    photo: ""
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <DateInput changeDate={this.changeDate} />
     </div>
   );
 }
