18aaa724e5d51c1c057ff4186c5076020d13a23f
Ambrose <ambrosehundal0082@gmail.com>
2021-12-25 21:02:19 -0800

    fixed bug and passing of props to display observatory info

--- src/components/ObservatoryInfo.js
+++ src/components/ObservatoryInfo.js
@@ -4,12 +4,9 @@ import React from "react";
 const ObservatoryInfo = ({info}) => {
     return (
         <div className="location-info">
-            <h2>{info.name}</h2>
-            <p>{info.link}</p>
-            <p>{info.instagram}</p>
-           
-            <ul>
-            </ul>
+            <h5>{info.name}</h5>
+            <p><a href={info.link}>See Website </a></p>
+            <p><a href={info.instagram}>Instagram</a></p>
         </div>
     )
 }
