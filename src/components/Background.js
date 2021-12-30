280548e9667e76f164141fa5b731ae946ce605e1
Ambrose <ambrosehundal0082@gmail.com>
2021-12-30 13:42:09 -0800

    css changes to animation on homepage

--- src/components/Background.js
+++ src/components/Background.js
@@ -5,14 +5,26 @@ import StarfieldAnimation from 'react-starfield-animation'
 class Background extends Component {
   render () {
     return (
+      <div>
+       
+
+      
       <StarfieldAnimation
         style={{
           position: 'relative',
           width: '100%',
           height: '100vw',
           color:"black",
+          backgroundColor: "black"
         }} numParticles = {1000}
-      />
+      >
+  
+
+
+
+
+        </StarfieldAnimation>
+        </div>
       
     )
   }
