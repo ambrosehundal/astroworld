39f6787df79b72bd00f07bb4bd8abeda9f42dd91
Ambrose <ambrosehundal0082@gmail.com>
2022-01-06 21:05:23 -0800

    search bar added to make api call

--- src/components/NASA/NASAImageSeachBar.js
+++ src/components/NASA/NASAImageSeachBar.js
@@ -4,6 +4,12 @@ const NASAImageSearchBar = () => {
     return (
         <div className="container">
         <h1>NASA Image Library</h1>
+        <input
+        type="text"
+        placeholder="Search"
+        value={null}
+        onChange={null}
+      />
         
         </div>
     )
