1836d12c128c71c42163bd9de44b0abd94138ea3
Ambrose <ambrosehundal0082@gmail.com>
2022-01-10 17:35:12 -0800

    description added to images

--- src/components/NASA/NASAImageSearchResults.js
+++ src/components/NASA/NASAImageSearchResults.js
@@ -4,22 +4,27 @@ import React from "react";
 const NASAImageSearchResults = props => {
     const imageLinks = [];
 
+    let imageMetadata;
     if(props.images){
         for(const [key, value] of Object.entries(props.images)){
-            imageLinks.push(value.links[0].href);
+            imageMetadata = [value.data[0].title, value.links[0].href];
+            imageLinks.push(imageMetadata);
         }
 
-    }
-
-    
-
-    console.log(imageLinks);
-
-    
+    }    
     return (
         <div className="container">
+            <div className="row">
             <div className="col-md-12">
-               {imageLinks.map(image => <img src={image}/>)}
+               {imageLinks.map(item => 
+
+                   <div className="row"> 
+                   <h6>{item[0]}</h6>
+                   <img src={item[1]}/>
+                   </div>
+
+               )}
+            </div>
             </div>
         
         </div>
